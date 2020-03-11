ba842704b5d614de76ded87b36850884156bb0ce
Oihana1 <61787900+Oihana1@users.noreply.github.com>
2020-03-11 14:18:07 +0100

    Update funtzioak.py

--- funtzioak.py
+++ funtzioak.py
@@ -1,17 +1,15 @@
-def kenketa(num1,num2):
+def Kenketa(num1,num2):
     if(num1>numb2):
         ken=numb1-numb2
     else:
         ken=numb2-numb1
     return ken
-def biderketa(num1,num2):
+def Biderketa(num1,num2):
     bider=num1*num2
     return bider
-def zatiketa(num1,num2):
+def Zatiketa(num1,num2):
      if(num1>numb2):
         zati=numb1/numb2
     else:
         zati=numb2/numb1
     return zati
-def berreketa(num1,num2):
-    if()
